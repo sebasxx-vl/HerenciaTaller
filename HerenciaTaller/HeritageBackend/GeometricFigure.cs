94c3599155a79f10753b66c0076360d02aa18cfe
Sebastian López <sebasgal608@gmail.com>
2026-03-06 16:21:04 -0500

    Created class abstract geometric figure

---
+++ HerenciaTaller/HeritageBackend/GeometricFigure.cs
@@ -0,0 +1,25 @@
+﻿namespace HeritageBackend;
+
+public abstract class GeometricFigure
+{
+    // Constructors
+    protected GeometricFigure(string name)
+    {
+        Name = name;
+    }
+
+    // Properties
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    // Methods
+    public abstract float GetArea();
+    public abstract float GetPerimeter();
+    public override string ToString()
+    {
+        return $"Figure: {Name}";
+    }
+}
