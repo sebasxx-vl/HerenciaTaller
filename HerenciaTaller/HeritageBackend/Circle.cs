952045a0b2ecc4229ab291ec7c25087ae78254f9
Sebastian López <sebasgal608@gmail.com>
2026-03-06 16:33:46 -0500

    Create class circle

---
+++ HerenciaTaller/HeritageBackend/Circle.cs
@@ -0,0 +1,45 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HeritageBackend;
+
+public class Circle : GeometricFigure
+{
+    // Fields
+    private float _r;
+
+    // Constructors
+    public Circle(float r) : base("Circle")
+    {
+        R = r; 
+    }
+
+    // Properties
+    public float R
+    {
+        get { return _r; }
+        set { _r = ValidateR(value); }
+    }
+
+    // Methods
+    public override float GetArea()
+    {
+        return MathF.PI * _r * _r;
+    }
+
+    public override float GetPerimeter()
+    {
+        return 2 * MathF.PI * _r;
+    }
+
+    private float ValidateR(float r)
+    {
+        if (r <= 0)
+            throw new ArgumentException("El radio debe ser mayor que 0");
+        return r;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Radio: {_r}, Área: {GetArea():F2}";
+    }
+}
