bb535af91427fca305361ec346142bccf784b41a
Sebastian López <sebasgal608@gmail.com>
2026-03-06 17:06:56 -0500

    Create class Kite

---
+++ HerenciaTaller/HeritageBackend/Kite.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageBackend;
+
+public class Kite : Rhombus
+{
+    // Fields
+    private float _b;
+
+    // Constructors
+    public Kite(float d1, float d2, float b) : base(d1, d2)
+    {
+        Name = "Kite";
+        B = b;
+    }
+
+    // Properties
+    public float B
+    {
+        get { return _b; }
+        set { _b = ValidateB(value); }
+    }
+
+    // Methods
+    public override float GetArea()
+    {
+        return D1 * D2 / 2;
+    }
+
+    public override float GetPerimeter()
+    {
+        return 2 * (D1 + B)/ 2;
+    }
+
+    private float ValidateB(float b)
+    {
+        if (b <= 0)
+            throw new ArgumentException("La diagonal 2 debe ser mayor que 0");
+        return b;
+    }
+}
